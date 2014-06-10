3d48b41a2f07346be0b84ea1a08df051ff5ddc10
Ben Wu <wubin28@gmail.com>
2014-06-10 22:10:53 +0800

    Added the original exercise code class Alarm and Sensor with a trivial test. Deleted class App and AppTest generated by Maven.

---
+++ tpms/src/main/java/kata/tpms/Alarm.java
@@ -0,0 +1,28 @@
+package kata.tpms;
+
+/**
+ * Created by benwu on 14-6-10.
+ */
+public class Alarm {
+    private final double LowPressureThreshold = 17;
+    private final double HighPressureThreshold = 21;
+
+    private Sensor sensor = new Sensor();
+
+    private boolean alarmOn = false;
+
+    public void check()
+    {
+        double psiPressureValue = sensor.popNextPressurePsiValue();
+
+        if (psiPressureValue < LowPressureThreshold || HighPressureThreshold < psiPressureValue)
+        {
+            alarmOn = true;
+        }
+    }
+
+    public boolean isAlarmOn()
+    {
+        return alarmOn;
+    }
+}
---
+++ tpms/src/main/java/kata/tpms/Sensor.java
@@ -0,0 +1,26 @@
+package kata.tpms;
+
+import java.util.Random;
+
+/**
+ * Created by benwu on 14-6-10.
+ */
+public class Sensor {
+    public static final double OFFSET = 16;
+
+    public double popNextPressurePsiValue()
+    {
+        double pressureTelemetryValue;
+        pressureTelemetryValue = samplePressure( );
+
+        return OFFSET + pressureTelemetryValue;
+    }
+
+    private static double samplePressure()
+    {
+        // placeholder implementation that simulate a real sensor in a real tire
+        Random basicRandomNumbersGenerator = new Random(42);
+        double pressureTelemetryValue = 6 * basicRandomNumbersGenerator.nextDouble() * basicRandomNumbersGenerator.nextDouble();
+        return pressureTelemetryValue;
+    }
+}
