2a04a4f7644f67509734e02358967ba254bb0572
Ben Wu <wubin28@gmail.com>
2014-06-12 18:58:38 +0800

    Created method FakeSensor.fakeNextPressurePsiValue().

--- tpms/src/main/java/kata/tpms/FakeSensor.java
+++ tpms/src/main/java/kata/tpms/FakeSensor.java
@@ -4,4 +4,7 @@ package kata.tpms;
  * Created by benwu on 14-6-12.
  */
 public class FakeSensor implements Detector {
+    public void fakeNextPressurePsiValue(double nextPressurePsiValue) {
+        
+    }
 }
