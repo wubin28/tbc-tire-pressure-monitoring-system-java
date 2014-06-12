c136a8cd9db6dec4420f14e15d548863fc96d024
Ben Wu <wubin28@gmail.com>
2014-06-12 19:36:06 +0800

    Finished TODO: a pressure value outside the range should raise the alarm.

--- tpms/src/test/java/kata/tpms/AlarmTest.java
+++ tpms/src/test/java/kata/tpms/AlarmTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by benwu on 14-6-10.
@@ -23,7 +24,20 @@ public class AlarmTest {
         assertFalse(alarm.isAlarmOn());
     }
 
-    // TODO-acceptance-test: a pressure value outside the range should raise the alarm
+    @Test
+    public void a_pressure_value_outside_the_range_should_raise_the_alarm() {
+        // Arrange
+        FakeSensor fakeSensor = new FakeSensor();
+        fakeSensor.fakeNextPressurePsiValue(Alarm.HIGH_PRESSURE_THRESHOLD + 1);
+        Alarm alarm = new Alarm(fakeSensor);
+
+        // Act
+        alarm.check();
+
+        // Assert
+        assertTrue(alarm.isAlarmOn());
+    }
+
     // TODO-acceptance-test: a normal pressure value after a value outside the range should not stop the alarm
     // TODO-new-feature: the alarm will be turned off before each checking of pressure
 }
