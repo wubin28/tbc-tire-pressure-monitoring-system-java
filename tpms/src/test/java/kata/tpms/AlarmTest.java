05a6329447f4518099ddecb77b32df87512eae15
Ben Wu <wubin28@gmail.com>
2014-07-31 14:28:43 +0800

    Working on TODO: a normal pressure value should not raise the alarm. Wrote a test a_normal_pressure_value_should_not_raise_the_alarm() with an Assert part.

--- tpms/src/test/java/kata/tpms/AlarmTest.java
+++ tpms/src/test/java/kata/tpms/AlarmTest.java
@@ -2,18 +2,20 @@ package kata.tpms;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by benwu on 14-6-10.
  */
 public class AlarmTest {
+
+    // TODO-user-intent-test-working-on: a normal pressure value should not raise the alarm
     @Test
-    public void a_trivial_test() {
-        assertEquals(5, 2 +3 );
+    public void a_normal_pressure_value_should_not_raise_the_alarm() {
+        // Assert
+        assertFalse(alarm.isAlarmOn());
     }
 
-    // TODO-user-intent-test: a normal pressure value should not raise the alarm
     // TODO-user-intent-test: a pressure value outside the range should raise the alarm
     // TODO-user-intent-test: a normal pressure value after a value outside the range should not stop the alarm
 
