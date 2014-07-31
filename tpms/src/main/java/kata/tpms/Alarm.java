2141fba2a939138a6c6ef9c193d8621406a8396b
Ben Wu <wubin28@gmail.com>
2014-07-31 14:13:26 +0800

    Added TODO: Depending on a concrete Sensor violates the Dependency Inversion Principle and Open-Closed Principle.

--- tpms/src/main/java/kata/tpms/Alarm.java
+++ tpms/src/main/java/kata/tpms/Alarm.java
@@ -7,6 +7,7 @@ public class Alarm {
     private final double LowPressureThreshold = 17;
     private final double HighPressureThreshold = 21;
 
+    // TODO: Depending on a concrete Sensor violates the Dependency Inversion Principle and Open-Closed Principle
     private Sensor sensor = new Sensor();
 
     private boolean alarmOn = false;
