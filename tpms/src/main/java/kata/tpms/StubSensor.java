7823394ed120171f6b44651f6f6475d54ea26d36
Ben Wu <wubin28@gmail.com>
2014-07-31 15:17:30 +0800

    Fixed compiler error in test: Made class StubSensor implement interface Transducer.

--- tpms/src/main/java/kata/tpms/StubSensor.java
+++ tpms/src/main/java/kata/tpms/StubSensor.java
@@ -3,7 +3,7 @@ package kata.tpms;
 /**
  * Created by benwu on 14-7-31.
  */
-public class StubSensor {
+public class StubSensor implements Transducer {
     public void arrangeNextPressurePsiValue(double nextPressurePsiValue) {
 
     }
