4c5f7dd896d727c4aed82502f58b1ee3875e5f8d
Ben Wu <wubin28@gmail.com>
2014-07-31 16:00:14 +0800

    Working on TODO: a pressure value outside the range should raise the alarm. Wrote intention code for the Assert, Act and Arrange parts of the test a_pressure_value_outside_the_range_should_raise_the_alarm().

--- tpms/src/test/java/kata/tpms/AlarmTest.java
+++ tpms/src/test/java/kata/tpms/AlarmTest.java
@@ -3,6 +3,7 @@ package kata.tpms;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by benwu on 14-6-10.
@@ -23,7 +24,21 @@ public class AlarmTest {
         assertFalse(alarm.isAlarmOn());
     }
 
-    // TODO-user-intent-test: a pressure value outside the range should raise the alarm
+    // TODO-user-intent-test-working-on: a pressure value outside the range should raise the alarm
+    @Test
+    public void a_pressure_value_outside_the_range_should_raise_the_alarm() {
+        // Arrange
+        StubSensor stubSensor = new StubSensor();
+        stubSensor.arrangeNextPressurePsiValue(Alarm.HIGH_PRESSURE_THRESHOLD + 1);
+        Alarm alarm = new Alarm(stubSensor);
+
+        // Act
+        alarm.check();
+
+        // Assert
+        assertTrue(alarm.isAlarmOn());
+    }
+
     // TODO-user-intent-test: a normal pressure value after a value outside the range should not stop the alarm
 
     // TODO-new-feature: a normal pressure value after a value outside the range should stop the alarm
