ce45d8f8a7851153faef45f3328b66ae8f5a06a4
Ben Wu <wubin28@gmail.com>
2014-07-31 15:35:34 +0800

    Fixed compiler error: Made class Sensor implement interface Transducer.

--- tpms/src/main/java/kata/tpms/Sensor.java
+++ tpms/src/main/java/kata/tpms/Sensor.java
@@ -5,7 +5,7 @@ import java.util.Random;
 /**
  * Created by benwu on 14-6-10.
  */
-public class Sensor {
+public class Sensor implements Transducer {
     public static final double OFFSET = 16;
 
     public double popNextPressurePsiValue()
